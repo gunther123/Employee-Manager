ef3876060a0c9ee5d85a1a95837a3cb7e1fd57af
Steven Slocum <steven.t.slocum@gmail.com>
2021-11-14 17:09:09 -0600

    created full db schema for all tables and related appropriate tables

--- db/schema.sql
+++ db/schema.sql
@@ -3,12 +3,23 @@ CREATE DATABASE org;
 USE org;
 
 CREATE TABLE department (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id INTEGER PRIMARY KEY UNSIGNED AUTO_INCREMENT,
     dep_name VARCHAR(30) NOT NULL
 );
 
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY UNSIGNED AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
 CREATE TABLE employee (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL
+    id INTEGER PRIMARY KEY UNSIGNED AUTO_INCREMENT,
+    first_name VARCHAR(30) UNIQUE NOT NULL,
+    last_name VARCHAR(30) UNIQUE NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    manager_id INT UNSIGNED NOT NULL
 );
