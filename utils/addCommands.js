864f3b26586f74ce738d7a5fa980401cc0b0eed1
Steven Slocum <steven.t.slocum@gmail.com>
2021-11-19 23:33:19 -0600

    refactor

--- utils/addCommands.js
+++ utils/addCommands.js
@@ -18,42 +18,59 @@ addDept = () => {
         });
     };
 
+    function example(){
+        const roleSql = `SELECT dep_name, id FROM department`; 
+        const dept = connection.query(roleSql, (err, data) => {
+              if (err) throw err; 
+          
+              const deptQuery = data.map(({ dep_name, id }) => ({ name: dep_name, value: id }));
+              console.log(deptQuery)
+              return deptQuery;
+        })
+        deptArr = dept.map(({ dep_name, id }) => ({ name: dep_name, value: id }));
+        console.log(deptArr)
+
+    return dept;
+}
+
     addRole = () => {
         inquirer.prompt(addRolePrompt)
         .then(answer => {
             const param = [answer.role, answer.salary];
+            
+            const dept = example();
+            ///console.log(dept)
+            // const roleSql = `SELECT dep_name, id FROM department`; 
       
-            const roleSql = `SELECT dep_name, id FROM department`; 
-      
-            connection.query(roleSql, (err, data) => {
-              if (err) throw err; 
+            // connection.query(roleSql, (err, data) => {
+            //   if (err) throw err; 
           
-              const dept = data.map(({ dep_name, id }) => ({ name: dep_name, value: id }));
+            //   const dept = data.map(({ dep_name, id }) => ({ name: dep_name, value: id }));
       
-              inquirer.prompt([
-              {
-                type: 'list', 
-                name: 'dept',
-                message: "What department is this role in?",
-                choices: dept
-              }
-              ])
-        .then(deptChosen => {
-              const dept = deptChosen.dept;
-              param.push(dept);
+    //           inquirer.prompt([
+    //           {
+    //             type: 'list', 
+    //             name: 'dept',
+    //             message: "What department is this role in?",
+    //             choices: dept
+    //           }
+    //           ])
+    //     .then(deptChosen => {
+    //           const dept = deptChosen.dept;
+    //           param.push(dept);
   
-              const sql = `INSERT INTO roles (title, salary, department_id)
-                          VALUES (?, ?, ?)`;
+    //           const sql = `INSERT INTO roles (title, salary, department_id)
+    //                       VALUES (?, ?, ?)`;
   
-              connection.query(sql, param, (err, result) => {
-                if (err) throw err;
-                console.log('Added ' + answer.role + " to roles!"); 
+    //           connection.query(sql, param, (err, result) => {
+    //             if (err) throw err;
+    //             console.log('Added ' + answer.role + " to roles!"); 
   
-                viewRoles();
-         });
-       });
+    //             viewRoles();
+    //      });
+    //    });
      })
-    })
+    // })
 }
     
 module.exports = {addDept};
